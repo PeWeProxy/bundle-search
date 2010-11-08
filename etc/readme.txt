e4a9a28f62ea21d4eb7d17bb1929f53d4c55e25e
Pavol Sokol <p.sokol@gmail.com>
2010-11-08 20:41:17 +0100

    Change parser from mozilla to htmlcleaner

--- etc/readme.txt
+++
@@ -1,4 +0,0 @@
-Temporary are native libraries placed here.
-In windows is necessary to set system path variable to following directories:
-./etc/mozilla/win
-./etc/mozilla/win/mozilla
---
+++ src/sk/fiit/rabbit/adaptiveproxy/plugins/servicedefinitions/HtmlDomBuilderService.java
@@ -0,0 +1,10 @@
+package sk.fiit.rabbit.adaptiveproxy.plugins.servicedefinitions;
+
+import org.jdom.Document;
+
+import sk.fiit.peweproxy.services.ProxyService;
+
+public interface HtmlDomBuilderService extends ProxyService {
+	public Document getHTMLDom();
+
+}
