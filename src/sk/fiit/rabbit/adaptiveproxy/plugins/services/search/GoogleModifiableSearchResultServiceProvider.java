72aba64580b39fc4ae57e9b8e007c06acd3623b6
Michal Vallus <vallusmichal@gmail.com>
2010-12-15 02:16:30 +0100

    Change XPath according to new Google layout
    Google result page layout has changed slightly. Quite soon for me.

--- src/sk/fiit/rabbit/adaptiveproxy/plugins/services/search/GoogleModifiableSearchResultServiceProvider.java
+++ src/sk/fiit/rabbit/adaptiveproxy/plugins/services/search/GoogleModifiableSearchResultServiceProvider.java
@@ -16,7 +16,7 @@ import sk.fiit.rabbit.adaptiveproxy.plugins.services.search.instructions.SwapIns
 
 public class GoogleModifiableSearchResultServiceProvider implements ModifiableSearchResultServiceProvider {
 
-	private static final String resultsParentElementPath = "/html/body/div[@id='cnt']/div[@id='nr_container']/div[@id='center_col']/div[@id='res']/div[@id='ires']/ol";
+	private static final String resultsParentElementPath = "/html/body/div[@id='main']/div[@id='cnt']/div[@id='nr_container']/div[@id='center_col']/div[@id='res']/div[@id='ires']/ol";
 	private static final Logger logger = Logger.getLogger(GoogleModifiableSearchResultServiceProvider.class);
 	
 	private Document responseDom;
